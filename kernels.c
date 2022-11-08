c9e2b4dbf252c6be18a754661ef88d582c717267
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2022-11-08 11:22:02 +0100

    Add working implementation of smooth
    * Add initial setup for basic_smooth
    
    * Minor performance gain with memory aliasing and code motion
    
    * Improve performance by doing fewer loops in avg
    
    * Add changes to baseline

--- kernels.c
+++ kernels.c
@@ -456,8 +456,8 @@ static pixel avg(int dim, int i, int j, pixel *src)
 
     initialize_pixel_sum(&sum);
     for(ii = max(i-1, 0); ii <= min(i+1, dim-1); ii++) 
-	for(jj = max(j-1, 0); jj <= min(j+1, dim-1); jj++) 
-	    accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
+	    for(jj = max(j-1, 0); jj <= min(j+1, dim-1); jj++) 
+	        accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
 
     assign_sum_to_pixel(&current_pixel, sum);
     return current_pixel;
@@ -476,9 +476,124 @@ void naive_smooth(int dim, pixel *src, pixel *dst)
     int i, j;
 
     for (i = 0; i < dim; i++)
-	for (j = 0; j < dim; j++)
-	    dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
+	    for (j = 0; j < dim; j++)
+	        dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
 }
+
+
+/* 
+ * avg - Returns averaged pixel value at (i,j) 
+ */
+static pixel avg_basic(int dim, int i, int j, pixel *src) 
+{
+    int ii, red_acc = 0, green_acc = 0, blue_acc = 0, count = 0;
+    pixel current_pixel;
+
+    int max_row_idx = min(i+1, dim-1);
+    int max_col_idx = min(j+1, dim-1);
+    int min_row_idx = max(i-1, 0);
+    int min_col_idx = max(j-1, 0); 
+    // int row_diff = max_row_idx - min_row_idx;
+    int col_diff = max_col_idx - min_col_idx;
+
+    int idx = RIDX(min_row_idx, min_col_idx, dim);
+    pixel *p = src+idx;
+    if(col_diff == 2) {
+
+        for(ii = min_row_idx; ii <= max_row_idx; ii++) {
+
+            red_acc += p->red;
+            green_acc += p->green;
+            blue_acc += p->blue;
+            
+            p++;
+            red_acc += p->red;
+            green_acc += p->green;
+            blue_acc += p->blue;
+
+            p++;
+            red_acc += p->red;
+            green_acc += p->green;
+            blue_acc += p->blue;
+
+            p += dim-2;
+            count+=3;
+        }
+    } else {
+
+        for(ii = min_row_idx; ii <= max_row_idx; ii++) {
+
+            red_acc += p->red;
+            green_acc += p->green;
+            blue_acc += p->blue;
+            
+            p++;
+            red_acc += p->red;
+            green_acc += p->green;
+            blue_acc += p->blue;
+
+            p += dim-1;
+            count+=2;
+        }
+    }
+
+    current_pixel.red = (unsigned short) (red_acc/count);
+    current_pixel.green = (unsigned short) (green_acc/count);
+    current_pixel.blue = (unsigned short) (blue_acc/count);
+    return current_pixel;
+}
+
+/*
+* basic smooth - ...
+*/
+char basic_smooth_descr[] = "basic_smooth: This is my first approach to a smooth implementation";
+void basic_smooth(int dim, pixel *src, pixel *dst) {
+
+    int i, j;
+
+
+    for (i = 0; i < dim; i++) {
+
+	    for (j = 0; j < dim; j++) {
+
+            dst[RIDX(i, j, dim)] = avg_basic(dim, i, j, src);
+        }
+    }
+}
+
+/* 
+ * initialize_pixel_sum - Initializes all fields of sum to 0 
+ */
+// static void initialize_pixel_sum_basic(pixel_sum *sum) 
+// {
+//     sum->red = sum->green = sum->blue = 0;
+//     sum->num = 0;
+//     return;
+// }
+
+/* 
+ * accumulate_sum - Accumulates field values of p in corresponding 
+ * fields of sum 
+ */
+// static void accumulate_sum_basic(pixel_sum *sum, pixel p) 
+// {
+//     sum->red += (int) p.red;
+//     sum->green += (int) p.green;
+//     sum->blue += (int) p.blue;
+//     sum->num++;
+//     return;
+// }
+
+/* 
+ * assign_sum_to_pixel - Computes averaged pixel value in current_pixel 
+ */
+// static void assign_sum_to_pixel_basic(pixel *current_pixel, pixel_sum sum) 
+// {
+//     current_pixel->red = (unsigned short) (sum.red/sum.num);
+//     current_pixel->green = (unsigned short) (sum.green/sum.num);
+//     current_pixel->blue = (unsigned short) (sum.blue/sum.num);
+//     return;
+// }
 
 /*
  * smooth - Your current working version of smooth. 
@@ -487,7 +602,7 @@ void naive_smooth(int dim, pixel *src, pixel *dst)
 char smooth_descr[] = "smooth: Current working version";
 void smooth(int dim, pixel *src, pixel *dst) 
 {
-    naive_smooth(dim, src, dst);
+    basic_smooth(dim, src, dst);
 }
 
 
@@ -500,6 +615,7 @@ void smooth(int dim, pixel *src, pixel *dst)
  *********************************************************************/
 
 void register_smooth_functions() {
+    add_smooth_function(&basic_smooth, basic_smooth_descr);
     add_smooth_function(&smooth, smooth_descr);
     add_smooth_function(&naive_smooth, naive_smooth_descr);
     /* ... Register additional test functions here */
